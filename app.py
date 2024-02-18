f0353986a41addc5a19d6928fecb7a50ababdb6b
Satya Sasi Vatsal <71326149+sasivatsal7122@users.noreply.github.com>
2024-02-19 00:12:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 async def get_target_base_url():
     # Update with the desired base URL of the target server
-    return "http://yts.mx"
+    return "https://yts.mx"
 
 
 @app.get("/proxy")
@@ -15,7 +15,7 @@ async def proxy(query: str, target_base_url: str = Depends(get_target_base_url))
 
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.get(target_url, allow_redirects=True)
+            response = await client.get(target_url)
             response.raise_for_status()
         except httpx.HTTPError as e:
             raise HTTPException(status_code=e.response.status_code, detail=str(e))
