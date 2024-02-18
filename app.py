ab2de9ce77d8b876225c909d7a80870e6a636efd
Satya Sasi Vatsal <71326149+sasivatsal7122@users.noreply.github.com>
2024-02-19 00:15:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from fastapi import FastAPI, HTTPException, Depends
 import httpx
 
 app = FastAPI()
-
-
 async def get_target_base_url():
-    # Update with the desired base URL of the target server
     return "https://yts.mx"
-
-
+    
 @app.get("/proxy")
 async def proxy(query: str, target_base_url: str = Depends(get_target_base_url)):
     target_url = f"{target_base_url}{query}"
@@ -19,11 +15,8 @@ async def proxy(query: str, target_base_url: str = Depends(get_target_base_url))
             response.raise_for_status()
         except httpx.HTTPError as e:
             raise HTTPException(status_code=e.response.status_code, detail=str(e))
-
-        # Assuming the response content is in JSON format
         return response.json()
-
+        
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
