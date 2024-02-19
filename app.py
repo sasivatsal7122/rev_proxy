259729705385e9b70efc80c7812310e0efb2b48a
Satya Sasi Vatsal <71326149+sasivatsal7122@users.noreply.github.com>
2024-02-19 11:45:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI, HTTPException, Depends
 import httpx
 
 app = FastAPI()
+
 async def get_target_base_url():
     return "https://yts.mx"
-    
+
 @app.get("/proxy")
 async def proxy(query: str, target_base_url: str = Depends(get_target_base_url)):
     target_url = f"{target_base_url}{query}"
@@ -15,8 +16,11 @@ async def proxy(query: str, target_base_url: str = Depends(get_target_base_url))
             response.raise_for_status()
         except httpx.HTTPError as e:
             raise HTTPException(status_code=e.response.status_code, detail=str(e))
+        if "/torrent/" in query:
+            return response
         return response.json()
-        
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
